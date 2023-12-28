eda644de14c86cf6d5f564c3f8c2f838fda08c13
Mobi <76767061+Mobiance@users.noreply.github.com>
2023-12-28 20:03:49 +0530

    added windows plugin

--- lua/themobi/plugins/ui.lua
+++ lua/themobi/plugins/ui.lua
@@ -1,5 +1,21 @@
 return {
     {
+        "anuvyklack/windows.nvim",
+        event = "WinNew",
+        dependencies = {
+            { "anuvyklack/middleclass" },
+            { "anuvyklack/animation.nvim", enabled = false },
+        },
+        keys = { { "<leader>m", "<cmd>WindowsMaximize<cr>", desc = "Zoom" } },
+        config = function()
+            vim.o.winwidth = 5
+            vim.o.equalalways = false
+            require("windows").setup({
+                animation = { enable = false, duration = 150 },
+            })
+        end,
+    },
+    {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
