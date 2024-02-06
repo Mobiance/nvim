20c57384f6a8f9bb58e5f7744f13b12b3d495591
Shubham Sharma <76767061+Mobiance@users.noreply.github.com>
2024-02-06 09:32:04 +0530

    Upadat harpoon plugin keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -9,6 +9,5 @@ vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:lis
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-
-vim.keymap.set("n", "<C-i>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-y>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
